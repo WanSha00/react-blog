fcedb636a9c7272ef4b88dcbceca699a315c0b8c
WanSha00 <88984549+WanSha00@users.noreply.github.com>
2024-04-13 19:31:54 +0800

    done login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,21 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <>
+      <div className="login">
+        <span className="loginTitle">Login</span>
+        <form className="loginForm">
+            <label>Email</label>
+            <input type="text" name="" id="" placeholder="Enter your email..."/>
+            <label>Password</label>
+            <input type="text" name="" id="" placeholder="Enter your password..."/>
+            <button className="loginButton">Login</button>
+        </form>
+        <button className="loginRegisterButton">Register</button>
+      </div>
+    </>
+  );
+}
+
+export default Login;
